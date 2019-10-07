3ce1aa0de1221a52d87140b7984fcd90c1a64fc3
mreber <mreber@saic.edu>
2019-10-07 18:13:41 -0500

    lol

---
+++ s10/js.js
@@ -0,0 +1,74 @@
+// console.log("hello faggots");
+
+// console.warn("faggots beware");
+
+console.error("faggot error");
+
+// // let num; 
+
+// //declaring a variabel ^
+
+// let num = 4; //initializing a variable
+// num = 4;
+
+// console.log(num);
+
+// //strings
+// console.log(" this \n is \n a \n string")
+// let myString = " \n \n \n contacgtinationa string in a variable\n \n \n"
+
+// console.log(myString);
+
+// //combine strings together
+
+// let name = 'doog';
+// let nails = 'orange';
+
+// console.log("my namd is " + name + " and i am ");
+
+// console.log(`my name is ${name} my`)
+
+// let combinedString = name + nails;
+
+// console.log(combinedString)
+
+// for (let i = 0; i < 10; i++) {
+// console.log(i)
+
+// }
+
+// let j = 0;
+
+// while(j < 10);
+// {
+//     console.log(j);
+//     j++;
+// }
+
+
+//conditional statements
+
+let coolNum = 20;
+
+if(coolNum < 20){
+
+    console.log("this number sucks ass");
+}
+else if(coolNum > 20) {
+    console.log("this number is too cool")
+}
+else {
+    console.log("this number is cool");
+}
+
+const myText = document.getElementById("JS");
+
+let x = setInterval(function() {
+
+
+myText.innerHTML = i;
+i++;
+
+},.1)
+
+i = 0;
